05d63d49b562f8e43bffb1c92f9ac3ed4a4d6345
dicky1207 <dickyimansyah007@gmail.com>
2024-11-19 10:14:43 +0700

    update

--- resources/views/auth/loginadmin.blade.php
+++ resources/views/auth/loginadmin.blade.php
@@ -64,13 +64,18 @@
               <div class="mb-2">
                 <label class="form-label">Password</label>
                 <div class="input-group input-group-flat">
-                  <input type="password" name="password" id="password" class="form-control" placeholder="Masukkan Password" autocomplete="off">
-                  <span class="input-group-text">
-                    <a href="#" class="link-secondary" data-bs-toggle="tooltip" id="togglePassword">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill=" none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
-                    </a>
-                  </span>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Masukkan Password" autocomplete="off">
+                    <span class="input-group-text">
+                        <a href="#" class="link-secondary" data-bs-toggle="tooltip" id="togglePassword">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="eyeOffIcon">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" /><path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" /><path d="M3 3l18 18" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" style="display: none;" id="eyeIcon">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M12 4c4.29 0 7.863 2.429 10.665 7.154l.22 .379l.045 .1l.03 .083l.014 .055l.014 .082l.011 .1v.11l-.014 .111a.992 .992 0 0 1 -.026 .11l-.039 .108l-.036 .075l-.016 .03c-2.764 4.836 -6.3 7.38 -10.555 7.499l-.313 .004c-4.396 0 -8.037 -2.549 -10.868 -7.504a1 1 0 0 1 0 -.992c2.831 -4.955 6.472 -7.504 10.868 -7.504zm0 5a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" />
+                            </svg>
+                        </a>
+                    </span>
                 </div>
               </div>
               <div class="form-footer">
@@ -88,15 +93,19 @@
     <script>
       const togglePassword = document.getElementById('togglePassword');
       const passwordInput = document.getElementById('password');
-
+      const eyeIcon = document.getElementById('eyeIcon');
+      const eyeOffIcon = document.getElementById('eyeOffIcon');
+  
       togglePassword.addEventListener('click', function (e) {
+          e.preventDefault();
           // Toggle the type attribute
           const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
           passwordInput.setAttribute('type', type);
-          // Toggle the eye slash icon
-          this.querySelector('svg').classList.toggle('icon-eye-off');
-          e.preventDefault();
+          
+          // Toggle the icons
+          eyeIcon.style.display = type === 'password' ? 'none' : 'block';
+          eyeOffIcon.style.display = type === 'password' ? 'block' : 'none';
       });
-  </script>
+    </script>
   </body>
 </html>
