6de51194077d22ff9246580ec385bc43e0ac408d
dicky1207 <dickyimansyah007@gmail.com>
2024-12-23 12:14:15 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepartemenController;
